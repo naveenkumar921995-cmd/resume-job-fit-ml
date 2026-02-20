10a61a8d5e919d97d09fcf344e34956b7e00ac1c
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-20 21:44:52 +0530

    Create operations.py

---
+++ core/job_database/operations.py
@@ -0,0 +1,5 @@
+OPERATIONS = {
+    "Operations Manager": ["process optimization", "logistics"],
+    "Project Manager": ["agile", "scrum", "jira"],
+    "Program Manager": ["stakeholder management"],
+}
