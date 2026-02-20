83c3147345fd812bdd039bb88f084dbff442e9cb
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-20 21:43:20 +0530

    Create marketing.py

---
+++ core/job_database/marketing.py
@@ -0,0 +1,7 @@
+MARKETING = {
+    "Digital Marketing Manager": ["seo", "sem", "google ads"],
+    "Content Strategist": ["content writing", "seo"],
+    "Social Media Manager": ["instagram", "facebook ads"],
+    "Brand Manager": ["branding", "campaigns"],
+    "SEO Specialist": ["seo", "analytics"],
+}
