024bbc197b7b060142b7df6348ff44f86532dbfa
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-20 21:31:41 +0530

    Update job_roles.py

--- core/job_roles.py
+++ core/job_roles.py
@@ -1,1 +1,21 @@
-
+JOB_ROLES = {
+    "Data Science": {
+        "Data Analyst": [
+            "python", "pandas", "numpy", "excel", "sql", "eda"
+        ],
+        "Machine Learning Engineer": [
+            "python", "sklearn", "feature engineering", "deployment", "mlops"
+        ],
+        "NLP Engineer": [
+            "nlp", "transformers", "bert", "text processing", "genai"
+        ]
+    },
+    "Software Development": {
+        "Backend Developer": [
+            "python", "django", "flask", "api", "sql"
+        ],
+        "Frontend Developer": [
+            "react", "javascript", "html", "css"
+        ]
+    }
+}
