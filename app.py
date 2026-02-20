8946b6fbeef497e6985c2f126701d7b0e14d4133
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-20 22:11:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
 from core.job_roles import JOB_ROLES
 from core.resume_parser import parse_resume
