916884fca73c6b6d8d5fb2e00ffbc136599103b0
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-20 21:41:54 +0530

    Create software_dev.py

---
+++ core/job_database/software_dev.py
@@ -0,0 +1,9 @@
+SOFTWARE_DEV = {
+    "Backend Developer": ["python", "django", "flask", "api"],
+    "Frontend Developer": ["react", "javascript", "html", "css"],
+    "Full Stack Developer": ["react", "node", "sql", "api"],
+    "Mobile Developer": ["android", "ios", "flutter"],
+    "Game Developer": ["unity", "c#", "game physics"],
+    "Embedded Engineer": ["c", "microcontrollers"],
+    "QA Engineer": ["selenium", "testing"],
+}
